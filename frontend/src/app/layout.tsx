56c5d1055e9c148d63c2946ec9c83d1ea9df5018
Ivan Wu <ivanwu@unc.edu>
2024-06-07 13:30:38 +0200

    Implement working Navbar with Mantine, fix compatibility issue between Tailwind and Mantine

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { NavbarMinimal } from "./components/navigation/page";
+import { MantineProvider } from "@mantine/core";
 import "./globals.css";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head></head>
+      <body>
+        <MantineProvider>
+          <NavbarMinimal />
+          <div className={inter.className} style={{ marginLeft: '80px' }}>
+            {children}
+          </div>
+        </MantineProvider>
+      </body>
     </html>
   );
 }
