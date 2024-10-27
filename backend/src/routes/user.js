6f1d9df14469f9c15892ffed814b0e32f20f8684
Ivan Wu <ivanwu@unc.edu>
2024-10-26 23:41:26 -0400

    Update user model to include preferences in preparation for profile section

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllUsers, handleNewUser, getUserById, updateUser, deleteUser} = require('../controllers/userController');
+const { getAllUsers, handleNewUser, getUserById, updateUser, deleteUser, getUserProfile, updateUserProfile} = require('../controllers/userController');
 const router = express.Router();
 const verifyJWT = require('../middlewares/verifyJWT');
 
@@ -8,5 +8,9 @@ router.post('/', handleNewUser);
 router.get('/:id', getUserById);
 router.put('/:id', updateUser);
 router.delete('/:id', deleteUser);
+
+// profile route
+router.get('/profile', verifyJWT, getUserProfile);
+router.put('/profile', verifyJWT, updateUserProfile);
 
 module.exports = router;
